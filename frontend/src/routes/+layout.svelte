fcada35bed499e5fb62c7269f262a4119c0f7e20
oSethoum <os.sethoum@gmail.com>
2023-07-27 19:24:11 +0100

    initial commit

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="h-screen bg-white">
+	<slot />
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="bg-white">
+	<h1>Welcome to SvelteKit Here</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
